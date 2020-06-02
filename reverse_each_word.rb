73326023e050625ff391dc3d53109cdf5a428cd2
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 19:40:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   final_array = []
   array = sentence.split(" ")
   array.each do |word|
-    final_array << "word.reverse"
+    final_array << word.reverse
   end 
   final_array.join(" ")
 end
